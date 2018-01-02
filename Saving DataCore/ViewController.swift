ccf27e21b1090d519e5f66c7e3a7a52105ac18de
lucasnorac <lucas_albarello_92@hotmail.com>
2018-01-02 21:37:47 -0200

    Initial Commit

---
+++ Saving DataCore/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Saving DataCore
+//
+//  Created by Lucas Caron Albarello on 02/01/2018.
+//  Copyright © 2018 Lucas Caron Albarello. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
